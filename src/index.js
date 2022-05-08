f57c6860dd8652276587ca611ff89fa0f1896635
= <=>
2022-05-08 17:41:35 +0200

    pridany react router a podstranky

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./Views/Home";
+import About from "./Views/About";
+import Page404 from "./Views/404";
 
-const container = document.getElementById("root");
+const container = document.querySelector("app-layout");
 const root = ReactDOM.createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<Page404 />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
