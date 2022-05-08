6a1ece55418e1947a12665f50bea743c69740a40
= <=>
2022-05-08 16:00:12 +0200

    zjednodusenie kostry aplikacie

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>
+          Edit <code>src/App.js</code> and save to reload.
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
